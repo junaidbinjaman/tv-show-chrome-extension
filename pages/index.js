d1ed16ed760fca4997d6bd66794d10cb3c7455d2
Junaid Bin Jaman <me@junaidbinjaman.com>
2023-02-25 16:05:07 +0600

    MUI is installed and added a btn on index.js to make sure it's working cirrectly

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, World</h1>
+      <Button variant="contained" >Hello, I am here</Button>
     </div>
   );
 }
