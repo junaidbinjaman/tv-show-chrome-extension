b73a3ad2862e81d8cc97f17990712d3a0d68ea48
junaidbinjaman <me@junaidbinjaman.com>
2024-05-03 06:41:45 +0600

    T

--- background.js
+++ background.js
@@ -1,35 +1,36 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    chrome.contextMenus.create({
-        title: "Test context menu",
-        id: "testcontextmenu1",
-        contexts: ["page", "selection"]
+  chrome.contextMenus.create({
+    title: 'Test context menu',
+    id: 'testcontextmenu1',
+    contexts: ['page', 'selection'],
+  });
+
+  chrome.contextMenus.onClicked.addListener((event) => {
+    chrome.tabs.create({
+      url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm`,
     });
+  });
 
-    chrome.contextMenus.onClicked.addListener((event) => {
-        console.log(event.selectionText);
-    })
+  chrome.contextMenus.create({
+    title: 'Test context menu 1',
+    id: 'testcontextmenu2',
+    parentId: 'testcontextmenu1',
+    contexts: ['page', 'selection'],
+  });
 
-    chrome.contextMenus.create({
-        title: "Test context menu 1",
-        id: "testcontextmenu2",
-        parentId: "testcontextmenu1",
-        contexts: ["page", "selection"]
-    });
+  chrome.contextMenus.create({
+    title: 'Test context menu 2',
+    id: 'testcontextmenu3',
+    parentId: 'testcontextmenu1',
+    contexts: ['page', 'selection'],
+  });
 
-    chrome.contextMenus.create({
-        title: "Test context menu 2",
-        id: "testcontextmenu3",
-        parentId: "testcontextmenu1",
-        contexts: ["page", "selection"]
-    });
+  chrome.contextMenus.create({
+    title: 'Test context menu 3',
+    id: 'testcontextmenu4',
+    parentId: 'testcontextmenu1',
+    contexts: ['page', 'selection'],
+  });
+});
 
-    chrome.contextMenus.create({
-        title: "Test context menu 3",
-        id: "testcontextmenu4",
-        parentId: "testcontextmenu1",
-        contexts: ["page", "selection"]
-    });
-})
-
-console.log('Background script running')
-  
+console.log('Background script running');
---
+++ content-script.js
@@ -0,0 +1,2 @@
+console.log('Hello from content scripts');
+document.querySelector("body > section > devsite-book-nav > div > input[type=text]").value = "Junaid Bin Jaman"
